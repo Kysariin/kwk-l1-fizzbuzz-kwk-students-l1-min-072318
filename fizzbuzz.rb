566f500a40081dc7c6f97add360604393e7db7d9
kate harris <kateharris03@gmail.com>
2018-07-24 18:29:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(x)
+  if x % 15 == 0
+    puts "fizzbuzz"
   if x % 3 == 0 
     puts "fizz"
   elsif x % 5 == 0
     puts "buzz"
-  elsif x % 15 == 0
-    puts "fizzbuzz"
+  
   else x % 15 != 0
     puts "nil"
   end
